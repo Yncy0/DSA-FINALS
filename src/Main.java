746b2225f5828ee1538ea39be8c1fa8f1ca38c25
Yncy0 <paalayancy@gmail.com>
2024-12-16 10:43:38 +0800

    done linear search

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] testArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        Scanner scanner = new Scanner(System.in);
+
+        int input = scanner.nextInt();
+
+        linearSearch(testArray, input);
+
+    }
+
+    private static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length-1; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
