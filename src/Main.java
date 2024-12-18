44620b5a6206e13545d3331f9e4a60b73e6d6e4b
Yncy0 <paalayancy@gmail.com>
2024-12-18 15:02:45 +0800

    WIP: Choices for user

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,80 @@
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        int[] testArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
+        int[] testArray = {51, 32, 43, 4, 5, 66, 87, 98, 39, 10};
         Scanner scanner = new Scanner(System.in);
-        System.out.println("DSA FINAL EXAM");
-        System.out.println(Arrays.toString(testArray));
-        System.out.print("Enter an item you want to find: ");
-        int input = scanner.nextInt();
 
-        linearSearch(testArray, input);
+        System.out.println("DSA FINAL EXAM");
+
+        while (true) {
+            selectionMenu(scanner, testArray);
+        }
 
     }
 
-    private static int linearSearch(int[] arr, int target) {
+    private static void selectionMenu(Scanner input, int arr[]) {
+        System.out.println("What do you want to do?: ");
+        System.out.println("[A]: SEARCH");
+        System.out.println("[B]: SORT");
+        System.out.println("[X]: CANCEL");
+        System.out.print("INPUT HERE >> ");
+        String choice = input.nextLine().toLowerCase();
+
+        while (true) {
+            if (choice.equals("a")) {
+                searchMenu(input, arr);
+            } else if (choice.equals("b")) {
+                sortMenu();
+            } else if (choice.equals("x")) {
+                return;
+            }
+//            } else {
+//                System.out.println("That is not the right input :(, please prefer on the references");
+//                break;
+//            }
+        }
+    }
+
+    private static void searchMenu(Scanner input, int arr[]) {
+        System.out.println("Please choose a SEARCHING ALGORITHM");
+        System.out.println("[A]: LINEAR SEARCH");
+        System.out.println("[Y]: BACK");
+        System.out.println("[X]: CANCEL");
+        System.out.print("INPUT HERE >> ");
+        String choice = input.nextLine().toLowerCase();
+
+        while (true) {
+            if (choice.equals("a")) {
+                System.out.println("HERE ARE THE ARRAYS");
+                System.out.println(Arrays.toString(arr));
+                System.out.println("Please choose a target number to search");
+                int target = input.nextInt();
+                linearSearch(arr, target);
+            }
+        }
+    }
+
+    private static void sortMenu() {
+        System.out.println("Please choose a SORTING ALGORITHM");
+    }
+
+    private static int linearSearch(int[] arr, int n) {
         for (int i = 0; i < arr.length-1; i++) {
-            System.out.println(i);
-            if (arr[i] == target) {
-                System.out.println("The target is in the index of " + i);
+            for (int j = 0; j <= i; j++) {
+                System.out.print(arr[j] + " ");
+            }
+            System.out.println();
+
+            if (arr[i] == n) {
+                System.out.println("The target " + n + " Is found at index of " + i);
                 return i;
             }
         }
 
+        System.out.println("The target is not in an array");
         return -1;
     }
 }
