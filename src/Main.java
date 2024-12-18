4773fbfb7b64c02ea75d4155a97520022de580e4
Yncy0 <paalayancy@gmail.com>
2024-12-18 14:04:40 +0800

    prototyping linear seacrh

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -5,7 +6,9 @@ public class Main {
         int[] testArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("DSA FINAL EXAM");
+        System.out.println(Arrays.toString(testArray));
+        System.out.print("Enter an item you want to find: ");
         int input = scanner.nextInt();
 
         linearSearch(testArray, input);
@@ -14,7 +17,9 @@ public class Main {
 
     private static int linearSearch(int[] arr, int target) {
         for (int i = 0; i < arr.length-1; i++) {
+            System.out.println(i);
             if (arr[i] == target) {
+                System.out.println("The target is in the index of " + i);
                 return i;
             }
         }
